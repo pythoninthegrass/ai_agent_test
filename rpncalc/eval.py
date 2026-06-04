f6202881d954d18668676c2a9d57aa4f30b5e679
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2026-06-04 16:01:35 -0500

    milestone 5: eval-arith

--- rpncalc/eval.py
+++ rpncalc/eval.py
@@ -15,4 +15,27 @@ class Eval:
 
     def eval(self):
         """Evaluate the RPN expression and return the result."""
+        for token_type, value in self.tokens:
+            if token_type in ("INT", "FLOAT"):
+                self.stack.append(value)
+            elif token_type == "OP":
+                self._apply_op(value)
+        
         return self.stack[-1] if self.stack else None
+
+    def _apply_op(self, op: str):
+        """Apply an operator to the top of the stack."""
+        if len(self.stack) < 2:
+            raise RpnError("stack underflow")
+        
+        b = self.stack.pop()
+        a = self.stack.pop()
+        
+        if op == "+":
+            self.stack.append(a + b)
+        elif op == "-":
+            self.stack.append(a - b)
+        elif op == "*":
+            self.stack.append(a * b)
+        elif op == "/":
+            self.stack.append(a / b)
