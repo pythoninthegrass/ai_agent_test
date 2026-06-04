bd86e647c1e7f0a2f7e9de928ccdea3ca8fc3172
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2026-06-04 16:00:16 -0500

    milestone 2: lex-ints

--- rpncalc/lexer.py
+++ rpncalc/lexer.py
@@ -1,5 +1,7 @@
 """Lexer for the rpncalc language."""
 
+import re
+
 
 class Lexer:
     """Tokenizes input strings into a list of tokens."""
@@ -11,4 +13,10 @@ class Lexer:
 
     def tokenize(self) -> list:
         """Return a list of tokens from the input text."""
+        # Pattern to match positive integers (sequences of digits)
+        pattern = r'\d+'
+        
+        for match in re.finditer(pattern, self.text):
+            self.tokens.append(("INT", int(match.group())))
+        
         return self.tokens
